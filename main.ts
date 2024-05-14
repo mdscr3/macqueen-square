239574c4c56d94ec17811dcc0c76b0bae38b9dfa
mdscr3 <160481957+mdscr3@users.noreply.github.com>
2024-05-14 16:05:47 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    for (let index = 0; index < 4; index++) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        basic.pause(2000)
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+        basic.pause(2000)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
